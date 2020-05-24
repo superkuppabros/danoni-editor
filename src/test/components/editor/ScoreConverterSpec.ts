7720ebb7e08d6de6923d06330c94369bb237c490
SKB <superkuppabros1995@gmail.com>
2020-05-24 20:07:04 +0900

    テスト修正

--- src/test/components/editor/ScoreConverterSpec.ts
+++ src/test/components/editor/ScoreConverterSpec.ts
@@ -12,41 +12,52 @@ describe("scoreConverter", () => {
       {
         label: 1,
         firstNum: 200,
-        bpm: 180
-      }
+        bpm: 180,
+      },
+      {
+        label: 3,
+        firstNum: 440,
+        bpm: 120,
+      },
     ],
     scores: [
       {
         notes: [[0], [], [96], [], []],
-        freezes: [[], [], [], [192], []]
+        freezes: [[], [], [], [192], []],
       },
       {
         notes: [[], [0, 48], [96], [], []],
-        freezes: [[], [], [], [0], []]
-      }
-    ]
+        freezes: [[], [], [], [0], []],
+      },
+      {
+        notes: [[], [], [], [], [0, 96]],
+        freezes: [[], [], [], [], []],
+      },
+    ],
   };
 
   it("frameDataへの変換が正しく出来る", () => {
     const expectedFrameData: FrameData[] = [
       {
         notes: [[200], [], [240], [], []],
-        freezes: [[], [], [], [280], []]
+        freezes: [[], [], [], [280], []],
       },
       {
         notes: [[], [360, 380], [400], [], []],
-        freezes: [[], [], [], [360], []]
-      }
+        freezes: [[], [], [], [360], []],
+      },
+      {
+        notes: [[], [], [], [], [440, 500]],
+        freezes: [[], [], [], [], []],
+      },
     ];
 
-    expect(scoreConverter.toFrameData(scoreData)).toStrictEqual(
-      expectedFrameData
-    );
+    expect(scoreConverter.toFrameData(scoreData)).toStrictEqual(expectedFrameData);
   });
 
   it("dosの出力が正しく出来る", () => {
     const expectedData =
-      "|left_data=200|down_data=360,380|up_data=240,400|right_data=|space_data=|frzLeft_data=|frzDown_data=|frzUp_data=|frzRight_data=280,360|frzSpace_data=|";
+      "|left_data=200|down_data=360,380|up_data=240,400|right_data=|space_data=440,500|frzLeft_data=|frzDown_data=|frzUp_data=|frzRight_data=280,360|frzSpace_data=|";
     expect(scoreConverter.convert(scoreData)).toBe(expectedData);
   });
 });
