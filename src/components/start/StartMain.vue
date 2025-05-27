9ddd048b623a92048d62e41a8b3e807315a4c090
SKB <superkuppabros1995@gmail.com>
2025-05-27 17:07:53 +0900

    build: 4.1.0

--- src/components/start/StartMain.vue
+++ src/components/start/StartMain.vue
@@ -33,7 +33,7 @@
         >
       </div>
 
-      <div id="start-version">ver 4.0.0 created by SKB</div>
+      <div id="start-version">ver 4.1.0 created by SKB</div>
     </div>
   </div>
 </template>
